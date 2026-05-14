6d477bb805c1125d7e255e1ba363be5c6eae7ca2
Akepati Sathish Reddy <sathishreddyakepati@gmail.com>
2026-05-14 21:34:29 +0530

    Improved statements by using (computer - you) output pattern

--- Project 1/main.py
+++ Project 1/main.py
@@ -13,23 +13,35 @@ youDict = {"R" : -1,"P" : 0,"S" : 1}
 reverseDict = {-1:"ROCK",0:"Paper",1:"Scissor"}
 you = youDict[youIn]
 print(f"You choose {reverseDict[you]} - Computer Choose {reverseDict[computer]}")
+result = computer - you
+
 if(computer == you):
     print("It's a Draw!")
+# else:
+#     if(computer ==-1 and you ==1): -2
+#       print("You lose!")
+#     elif(computer ==-1 and you==0):-1
+#       print("You won!")
+#     elif(computer == 0 and you ==-1):1
+#       print("You lose!")
+#     elif(computer == 0 and you==1):-1
+#       print("You win!")
+#     elif(computer == 1 and you == -1):2
+#       print("You win!")
+#     elif(computer== 1 and you== 0):1
+#       print("You lose!")
+#     else:
+#       print("Something went wrong!")
 else:
-    if(computer ==-1 and you ==1):
-      print("You lose!")
-    elif(computer ==-1 and you==0):
-      print("You won!")
-    elif(computer == 0 and you ==-1):
-      print("You lose!")
-    elif(computer == 0 and you==1):
-      print("You win!")
-    elif(computer == 1 and you == -1):
-      print("You win!")
-    elif(computer== 1 and you== 0):
-      print("You lose!")
+    if(result == -2 or result == 1 ):
+      print("You Lose!")
+
+    elif(result == -1 or result == 2):
+       print("You Won!")
     else:
-      print("Something went wrong!")
+       print("Something went wrong!")
+
+
     
 
 
